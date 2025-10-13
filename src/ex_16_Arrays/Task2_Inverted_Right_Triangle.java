8f2384baa29d6a7b10f2427d0a357299548182e8
Priyanka <priyankaindulkar032@gmail.com>
2025-10-13 18:42:54 +0530

    Renamed Java files to maintain sequential order

---
+++ src/ex_16_Arrays/Task2_Inverted_Right_Triangle.java
@@ -0,0 +1,14 @@
+package ex_16_Arrays;
+
+public class Task2_Inverted_Right_Triangle {
+    public static void main(String[] args) {
+        for(int i=5;i>=1;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
